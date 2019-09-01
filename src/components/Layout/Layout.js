0f288f5e5d98e51c0400c3c8733fc489ce6306c5
lakshay <ihybrid11@gmail.com>
2019-09-01 23:26:04 +0530

    added nav

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import AuxComponent from '../../hoc/AuxComponent';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
     <AuxComponent>
-        <div>
-            Toolbar, SideDrawer, BackDrop
-        </div>
+        <Toolbar />
+        <SideDrawer />
         <main className={classes.Content}>
             {props.children}
         </main>
